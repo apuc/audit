b213b99afa82ea6b649b278135d69c77efbd7e99
akosse <andrey_kosse@bk.ru>
2020-01-14 11:04:09 +0300

    show queue domain

--- frontend/modules/audit/views/audit/pending.php
+++ frontend/modules/audit/views/audit/pending.php
@@ -4,13 +4,22 @@ use yii\grid\GridView;
 use yii\data\ActiveDataProvider;
 
 echo "<h3>Сайты в очереди на аудит</h3><br>";
+//$queue = \common\models\AuditPending::find()->all();
+//foreach ($queue as $value)
+//$query = "SELECT site.name FROM site, audit_pending WHERE site.id =".$id;
 $dataProvider = new ActiveDataProvider(['query' => \common\models\AuditPending::find()]);
 echo GridView::widget([
     'dataProvider' => $dataProvider,
+    'columns' => [
+        'site.name'
+    ],
 ]);
 
 echo "<br><br><h3>Сайты в очереди на индексацию</h3><br>";
 $dataProvider = new ActiveDataProvider(['query' => \common\models\IndexingPending::find()]);
 echo GridView::widget([
     'dataProvider' => $dataProvider,
+    'columns' => [
+       'site.name'
+    ],
 ]);

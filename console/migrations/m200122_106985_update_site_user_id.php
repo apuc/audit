c36c7e1612a7ebf94dd1bab963db689f463e5743
akosse <andrey_kosse@bk.ru>
2020-01-22 17:15:25 +0300

    update site user_id migration 1

--- console/migrations/m200122_106985_update_site_user_id.php
+++ console/migrations/m200122_106985_update_site_user_id.php
@@ -2,12 +2,12 @@
 
 use yii\db\Migration;
 
-class m200122_106984_update_site_user_id extends Migration
+class m200122_106985_update_site_user_id extends Migration
 {
     public function safeUp()
     {
         $this->update('site', [
-            'user_id' => 2,
+            'user_id' => 1,
         ]);
     }
 
